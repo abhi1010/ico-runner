1cb7ad21746ee088da352e1a392cc349e4a90dbc
abhishek.pandey <abhishek.pandey@grasshopperasia.com>
2017-12-25 17:25:26 +0800

    deploy zeppelin crowdsale - working version

--- scripts/11_deploy_zeppelin_crowdsale.py
+++ scripts/11_deploy_zeppelin_crowdsale.py
@@ -1,7 +1,3 @@
-
-
-
-
 ### """Deploy crowdsale contract."""
 
 import base
@@ -9,8 +5,7 @@ from base import *
 
 print('CACHE[FIELDNAME.CONTRACT] = {}'.format(CACHE[FIELDNAME.CONTRACT]))
 
-CONTRACT_NAME = 'SaleAbhi'
-
+CONTRACT_NAME = 'Crowdsale'
 
 
 def utc_time():
@@ -23,31 +18,15 @@ def utc_time():
     t = (d - epoch).total_seconds()
     return t
 
-print('')
 
+print('Deploying: {}'.format(CONTRACT_NAME))
 
-chain, address, owner, days, minimum, verify = CHAIN, OWNER, OWNER, 1, 0.001, False
+chain, address, owner, days = CHAIN, OWNER, OWNER, 1
 
-# Parse command line args to crowdsale constructor args
-minimum = to_wei(0.001, "ether")
-starts_at = int(utc_time())
-freeze_ends_at = int(utc_time() + days * 24*3600)
+starts_at = int(utc_time() - 100)
+freeze_ends_at = int(utc_time() + days * 24 * 3600)
 
-
-# args = [starts_at, freeze_ends_at,
-#         1, 10000, 100000, OWNER]
-args = [OWNER]
-
-# CONTRACT_NAME = "Crowdsale"
-
-# CONTRACT_NAME = "UncappedAbhi"
-
-# This is configured in populus.json
-# We are working on a testnet
-print("Make sure {} chain is running, you can connect to it, or you'll get timeout".format(chain))
-
-CROWDSALE = None
-CROWDSALE_ADDRESS = None
+args = [starts_at, freeze_ends_at, to_wei(10, 'ETHER'), address]
 
 with project.get_chain(chain) as c:
 
@@ -55,30 +34,33 @@ with project.get_chain(chain) as c:
     print("Web3 provider is", web3.currentProvider)
     print("Deploy address is", address)
     if not address:
-        sys.exit("You need to explicitly give the address from where we are deploying from")
+        sys.exit(
+            "You need to explicitly give the address from where we are deploying from"
+        )
 
-    print("Deploy address balance is", from_wei(web3.eth.getBalance(address), "ether"))
+    print("Deploy address balance is", from_wei(
+        web3.eth.getBalance(address), "ether"))
 
     # Goes through geth account unlock process if needed
     if is_account_locked(web3, address):
         request_account_unlock(c, address, None)
 
-
-    # This does deployment with all dependencies linked in
-    print("Deploying contracts with args={}".format(args))
-    print('='*80)
+    print("Deploying contracts with args={}; address={}".format(args, address))
+    print('=' * 80)
     print()
-    crowdsale, txhash = c.provider.deploy_contract(
+    # crowdsale, txhash = c.provider.get_or_deploy_contract(
+    crowdsale, txhash = c.provider.get_or_deploy_contract(
         CONTRACT_NAME,
-        deploy_transaction={"from": address},
+        deploy_transaction={
+            "from": address,
+            # 'gas': 10000000,
+            # 'gasLimit': 1000000000
+        },
         deploy_args=args)
-#     print("Deploying crowdsale, tx hash is: ", txhash)
-    print("CrowdSale contract address is (CROWDSALE_ADDRESS): ", crowdsale.address)
-    CROWDSALE = crowdsale
-    CROWDSALE_ADDRESS = crowdsale.address
-    CACHE[FIELDNAME.CROWDSALE_ADDRESS] = CROWDSALE_ADDRESS
-
-
+    print("Deploying crowdsale, tx hash is: ", txhash)
+    print("CrowdSale contract address is (CROWDSALE_ADDRESS): ",
+          crowdsale.address)
+    CACHE[FIELDNAME.CROWDSALE_ADDRESS] = crowdsale.address
 
     print("All done! Enjoy your decentralized future.")
     save_cache()
